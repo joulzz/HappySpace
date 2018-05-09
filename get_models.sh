bc77e4cccd18a96959053e988d39340f770c1795
Suraj Pai <surajballambat@gmail.com>
2018-05-09 17:40:16 +0530

    #19 Changes for tinkerboard deploy

--- get_models.sh
+++ get_models.sh
@@ -1,6 +1,6 @@
 wget https://transfer.sh/ZIWAD/sentiment_net.data-00000-of-00001 https://transfer.sh/R9ZEw/sentiment_net.index https://transfer.sh/RdRbY/sentiment_net.meta https://transfer.sh/SAVbX/haarcascade_frontalface_default.xml  https://transfer.sh/miPqx/svm_model.pkl
 mkdir Models
 mkdir Models/sentiment_net
-mv sentiment_net* Models/sentiment_net
+mv sentiment_net.* Models/sentiment_net
 mv haarcascade* Models/
 mv svm_model.pkl Models/
