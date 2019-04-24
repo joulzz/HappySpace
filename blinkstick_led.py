b1b0367f6d20dbfb14cbf4fce2ef66e78132b5f9
Unknown <nayan.taurian@gmail.com>
2019-04-24 23:02:59 +0530

    #38 Configuring morph function

--- blinkstick_led.py
+++ blinkstick_led.py
@@ -3,4 +3,5 @@ from blinkstick import blinkstick
 def led_blink(color):
     led = blinkstick.find_first()
     led.set_mode(3)
-    led.blink(name=color,delay=10)
+    # led.blink(name=color,delay=10)
+    led.morph(name="random",duration=10000)
