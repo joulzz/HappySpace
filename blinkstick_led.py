3b22cde9cf89295c936336a94b68288889bced33
Unknown <nayan.taurian@gmail.com>
2019-05-09 00:28:52 +0530

    Error handling for blinkstick bug

--- blinkstick_led.py
+++ blinkstick_led.py
@@ -3,8 +3,11 @@ from blinkstick import blinkstick
 
 def main():
     try:
-        for led in blinkstick.find_all():
-            led.set_mode(3)
+        try:
+            for led in blinkstick.find_all():
+                led.set_mode(3)
+        except AttributeError:
+            return None
         while True:
             led.morph(name="random",duration=10000)
     except (KeyboardInterrupt, SystemExit):
