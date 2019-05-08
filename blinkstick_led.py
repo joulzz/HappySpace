09fe3e7f76bce25799b03fe898e79e821e7a73e8
Unknown <nayan.taurian@gmail.com>
2019-05-08 23:56:47 +0530

    Shifting functions to avoid Blinkstick bug

--- blinkstick_led.py
+++ blinkstick_led.py
@@ -3,8 +3,8 @@ from blinkstick import blinkstick
 
 def main():
     try:
-        led = blinkstick.find_first()
-        led.set_mode(3)
+        for led in blinkstick.find_all():
+            led.set_mode(3)
         while True:
             led.morph(name="random",duration=10000)
     except (KeyboardInterrupt, SystemExit):
