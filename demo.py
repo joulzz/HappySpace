c3780f7f4eb329d6082c39223b805f9542d73651
Unknown <nayan.taurian@gmail.com>
2019-06-09 04:11:48 +0530

    #39 Inverse, Pixelation and Grayscale

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 import cv2
+from PIL import Image
+import PIL.ImageOps
 
 img = cv2.imread('yesterday_test.jpg')
 img = cv2.resize(img,(640,480))
@@ -22,6 +24,24 @@ cv2.imshow('Median Filtering', median)
 #Bilateral filtering
 bf = cv2.bilateralFilter(img,9,75,75)
 cv2.imshow('BiLateral Filtering', bf)
+
+
+#Inverting Colors
+image = Image.open('yesterday_test.jpg')
+
+inverted_image = PIL.ImageOps.invert(image)
+
+inverted_image.save('invert.jpg')
+
+#Pixelate Images
+
+imgSmall = image.resize(size=(32,32),resample=Image.BILINEAR)
+result = imgSmall.resize(size=(640,480),resample=Image.NEAREST)
+result.save('pixelate.jpg')
+
+#Grayscale
+img = image.convert('LA')
+img.save('grayscale.jpg')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
