e2593968819fb56887ea524a0b96ded8ec842dda
Nayan Mehta <nayan.taurian@gmail.com>
2019-07-17 08:46:34 +0530

    Updated cos sim calculation

--- post_processing_reidentification.py
+++ post_processing_reidentification.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+from ast import literal_eval
 
 def cos_similarity(X, Y):
-    Y = Y.T    # (1, 256) x (256, n) = (1, n)
+    X = X.strip("[").strip("]")
+    Y = Y.strip("[").strip("]")
+    X = np.fromstring(X, sep=' ')
+    Y = np.fromstring(Y,sep=' ')
+    Y = np.transpose(Y)
+    print np.dot(X, Y)/(np.linalg.norm(X) * np.linalg.norm(Y, axis=0))
     return np.dot(X, Y)/(np.linalg.norm(X) * np.linalg.norm(Y, axis=0))
 
 
@@ -15,7 +21,9 @@ for (index, row) in output_df.iterrows():
         output_df.loc[index, 'ID'] = output_df.loc[index, 'Reference ID']
 
     else:
-        similarity = cos_similarity(output_df.loc[index,'Face_Vectors'], output_df.loc[index+1,'Face_Vectors'])
+        # output_df['Face_Vectors'] = pd.eval(output_df['Face_Vectors'])
+
+        similarity = cos_similarity(output_df.loc[index,'Face_Vectors'], output_df.loc[index-1,'Face_Vectors'])
         if similarity == 1:
             print ("Replaced ID at Index: ", index)
             output_df.loc[index, 'ID'] = output_df.loc[index - 1, 'ID']
