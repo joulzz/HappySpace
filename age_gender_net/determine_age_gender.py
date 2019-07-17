8b396022d0bb91831ec9c487dc68aa5d0bb6b94f
Nayan Mehta <nayan.taurian@gmail.com>
2019-07-17 10:27:35 +0530

    Added documentation for age-gender net

--- age_gender_net/determine_age_gender.py
+++ age_gender_net/determine_age_gender.py
@@ -11,20 +11,41 @@ class GAPredictor:
        Attributes
        ----------
        gender_list:
-           List to identify genders
+           Labels list for mapping between output vector and gender.
        ga_net:
-           Variable used to hold the network
+           Gender-Age network description that is sent to plugin to load
        ga_input_blob:
-           Variable used to feed input to the network
+           The name of the node in the network where the image is passed as input
        ga_out_blob:
+            The name of the node in the network where the result vector is received as outpuit
+
        ga_size:
+            Size of the image expected at the input node
        exec_ga_net:
+            The in-memory network loaded that executes a forward pass over the network
 
        Methods
        -------
 
        """
     def __init__(self, plugin, ga_model_xml):
+    
+        """ Description
+
+        Initializes and loads the gender-age prediction network into memory
+        :type self:
+        :param self:
+    
+        :type plugin: 
+        :param plugin: Inference Engine Plugin
+    
+        :type ga_model_xml: string
+        :param ga_model_xml: XML path of the ga xml description
+    
+        :raises:
+    
+        :rtype:
+        """    
         self.gender_list = ["female", "male"]
 
         ga_model_bin = os.path.splitext(ga_model_xml)[0] + ".bin"
@@ -45,7 +66,24 @@ class GAPredictor:
 
 
     def preprocess_image(self, face):
-        if face.shape[:-1] != (self.ga_size[2], self.ga_size[3]):
+    
+        """ Description
+
+        Preprocess the image by resizing and transposing as required by the Gender-Age prediction network in IE format.
+        :type self:
+        :param self:
+    
+        :type face: numpy array
+        :param face: Image representing crop area of the detected face. 
+    
+        :raises:
+
+        
+
+        :rtype face_frame:
+        :returns face_frame: Returns processed numpy array ready to be sent for inference
+
+        """    if face.shape[:-1] != (self.ga_size[2], self.ga_size[3]):
             face_frame = cv2.resize(face, (self.ga_size[3], self.ga_size[2]))
 
         face_frame = face_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
@@ -53,6 +91,24 @@ class GAPredictor:
 
     def predict(self, face_frame):
 
+    
+        """ Description
+
+        Runs the Gender Age Prediction on the input and provides the age, gender output
+        :type self:
+        :param self:
+    
+        :type face_frame:
+        :param face_frame: The numpy array returned by the preprocess_image function
+    
+        :raises:
+    
+        :rtype age: float
+        :returns age: Age in float format
+
+        :rtype age: string
+        :returns age: Gender as either "male" or "female"
+        """
         res = self.exec_ga_net.infer(inputs={self.ga_input_blob: face_frame})
         age = int(res['age_conv3'].reshape(-1) * 100)
         gender = self.gender_list[np.argmax(res['prob'].reshape(-1, 2))]
